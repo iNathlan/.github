df4e89c784c6cb6c33ab7c644e8ec3a53ca90017
𝙂𝙚𝙣𝙝𝙤 𝘼𝙠𝙖𝙙𝙞𝙤𝙣 ® <ogenhoanimation01@gmail.com>
2026-05-18 17:25:51 -0300

    V16

--- profile/windgets/update_rest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in profile/windgets/status.svg profile/windgets/team.svg profile/windgets/connect.svg; do
-  if ! grep -q "Plus Jakarta Sans" "$file"; then
-    sed -i "s|<style>|<style>\n    @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800\&display=swap');\n    text { font-family: 'Plus Jakarta Sans', system-ui, sans-serif !important; }|g" "$file"
-  fi
-done
