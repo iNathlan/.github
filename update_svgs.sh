82eab3d54c5e159859d0752bf074dfeaa9df92ce
𝙂𝙚𝙣𝙝𝙤 𝘼𝙠𝙖𝙙𝙞𝙤𝙣 ® <ogenhoanimation01@gmail.com>
2026-05-18 17:24:43 -0300

    V15

--- update_svgs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in profile/windgets/*.svg; do
-  # Replace main container backgrounds
-  sed -i 's/fill="#0d1117"/fill="transparent"/g' "$file"
-  # Replace card backgrounds
-  sed -i 's/fill="#161b22"/fill="transparent"/g' "$file"
-  # Remove border strokes if there are any random ones
-  # We might just leave the strokes for circles/icons because they are UI elements
-done
